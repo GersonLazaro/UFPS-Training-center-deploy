e0d35137c31ea1a3701daaaebe030ce19c515a8e
Angie M. Delgado <amelisdl@gmail.com>
2018-03-21 23:04:06 -0500

    Fix python compiler

--- backend/grader/config.js
+++ backend/grader/config.js
@@ -3,7 +3,7 @@
 class Config {
     constructor () {
       this.langs = ['Java', 'C++', 'Python']
-      this.compilers = ['javac /files/{folder}/Main.java', 'g++ /files/{folder}/Main.cpp -std=c++11 -o /files/{folder}/Main', '']
+      this.compilers = ['javac /files/{folder}/Main.java', 'g++ /files/{folder}/Main.cpp -std=c++11 -o /files/{folder}/Main', 'ls']
       this.runners = ['javaRunner.sh', 'cppRunner.sh', 'pythonRunner.sh']
       this.containers = ['javaSandbox', 'cppSandbox', 'pythonSandbox']
       this.filename = ['Main.java', 'Main.cpp', 'Main.py']
