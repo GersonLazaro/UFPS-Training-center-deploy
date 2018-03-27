36fb69d17ca16fa3c4f04fd4df663f2abae44ef3
Angie M. Delgado <amelisdl@gmail.com>
2018-03-26 19:53:27 -0500

    Update debugs

--- backend/services/socketsApi.js
+++ backend/services/socketsApi.js
@@ -11,32 +11,55 @@ var socketApi = {};
 socketApi.io = io;
 
 contest.on('connection', function(socket) {
-    console.log('User in contest mode')
+    console.log('***********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'              REGISTRO - MARATON              '
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+            )
 })
 
 platform.on('connection', function(socket){
     socket.on('register', function( user_id ){
         addSocket( user_id, socket.id )
     })
-    console.log('User in normal mode')
 });
 
-socketApi.notifySubmissionResult = function( user_id, problem_id, verdict ){
+socketApi.notifySubmissionResult = function( user_id, problem_id, verdict, problem_name ){
     getSocket( user_id, ( socket_id ) => {
+        console.log( 'Socket ENVIO: ' + socket_id )
         platform.to(socket_id).emit('new result',{
             user_id: user_id,
             problem_id: problem_id,
-            verdict: verdict
+            verdict: verdict,
+            problem_name: problem_name
         })
     })
 }
 
-socketApi.refreshScoreboard = function( user_id, problem_id, verdict, submission_id ){
+socketApi.refreshScoreboard = function( user_id, problem_id, verdict, submission_id, problem_name ){
+    console.log('***********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'                   CONTEST                    '
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+                +'**********************************************'
+            )
     contest.emit('new submission', {
         user_id: user_id,
         problem_id: problem_id,
         verdict: verdict,
-        submission_id: submission_id
+        submission_id: submission_id, 
+        problem_name: problem_name
     })
 }
 
@@ -46,6 +69,7 @@ function addSocket( user_id, socket_id ){
         { where: { id: user_id } }
     ).then((affectedRows) => {
     }).catch((err) => {
+        console.log('ADD SOCKET ERROR: ' + err )
     })
 }
 
@@ -54,7 +78,9 @@ function getSocket( user_id, success ){
     .then( (user) => {
         success( user.socket_id )
     })
-    .catch( (err) => {})
+    .catch( (err) => {
+        console.log('GET SOCKET ERROR: ' + err )
+    })
 }
 
 module.exports = socketApi;
